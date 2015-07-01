6eda4120edc2ce3ef1cb7e2a6d04e7b681f50d9e
brianhigh <bhigh@live.com>
2015-07-01 08:00:48 -0700

    Fixed spacing in headings.

--- suggested_reading.sh
+++ suggested_reading.sh
@@ -8,38 +8,38 @@ Brian High
 07/01/2015
 ' > $OUT
 
-echo 'This list was automatically generated from the Amazon API. It includes
-the top-5 search results for each search term, based on customer reviews.' \
+echo -e "This list was automatically generated from the Amazon API. It includes
+the top-5 search results for each search term, based on customer reviews.\n" \
     >> $OUT
 
-echo "## Information Systems" >> $OUT
+echo -e "## Information Systems\n" >> $OUT
 python amznsrch.py 'Computer Hardware' 'Computer Software' >> $OUT
 python amznsrch.py 'Systems Analysis' 'Project Management' >> $OUT
 python amznsrch.py 'Computer Networking' >> $OUT
 python amznsrch.py 'Information Security' 'Computer Security' >> $OUT
 
-echo "## Data Management" >> $OUT
+echo -e "## Data Management\n" >> $OUT
 python amznsrch.py 'Data Management' 'Database' 'SQL' >> $OUT
 python amznsrch.py 'Data Wrangling' 'Bad Data' >> $OUT
 python amznsrch.py 'R Web Scraping' 'Python Web Scraping' >> $OUT
 
-echo "## Programming" >> $OUT
+echo -e "## Programming\n" >> $OUT
 python amznsrch.py 'R Programming' 'RStudio' >> $OUT
 python amznsrch.py 'Python Programming' 'IPython' >> $OUT
 python amznsrch.py 'Regular Expressions' >> $OUT
 
-echo "## Version Control" >> $OUT
+echo -e "## Version Control\n" >> $OUT
 python amznsrch.py 'Version Control' 'GitHub' >> $OUT
 
-echo "## Data Analysis and Visualization" >> $OUT
+echo -e "## Data Analysis and Visualization\n" >> $OUT
 python amznsrch.py 'Data Analysis' >> $OUT
 python amznsrch.py 'R Visualization' 'Python Visualization' >> $OUT
 
-echo "## Bioinformatics" >> $OUT
+echo -e "## Bioinformatics\n" >> $OUT
 python amznsrch.py 'R Bioinformatics' 'Python Bioinformatics' >> $OUT
 python amznsrch.py 'Computational Biology' >> $OUT
 
-echo "## Buzzwords" >> $OUT
+echo -e "## Buzzwords\n" >> $OUT
 python amznsrch.py 'Big Data' >> $OUT
 python amznsrch.py 'Cloud Computing' >> $OUT
 python amznsrch.py 'Data Science' 'R Data Science' 'Python Data Science' >> $OUT
